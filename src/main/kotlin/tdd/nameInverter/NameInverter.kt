083346e1619500161798b01c47b144ea9eb14c62
zionlee0927 <zionlee0927@gmail.com>
2022-08-20 16:38:26 +0900

    feat: nameInverter init

--- src/main/kotlin/tdd/nameInverter/NameInverter.kt
+++ src/main/kotlin/tdd/nameInverter/NameInverter.kt
@@ -1,8 +1,5 @@
 package tdd.nameInverter
 
 class NameInverter {
-    fun inverter(name: String?): String{
-        if (name.isNullOrBlank()) return ""
-        return name
-    }
+
 }
--- src/test/kotlin/tdd/NameInverterTest.kt
+++ src/test/kotlin/tdd/NameInverterTest.kt
@@ -8,17 +8,7 @@ internal class NameInverterTest{
     private val nameInverter: NameInverter = NameInverter()
 
     @Test
-    fun given_null___returns_empty_string(){
-        assertThat(nameInverter.inverter(null)).isEqualTo("")
-    }
+    fun test(){
 
-    @Test
-    fun given_empty_string___returns_empty_string(){
-        assertThat(nameInverter.inverter("")).isEqualTo("")
-    }
-
-    @Test
-    fun given_simple_name___returns_simple_name(){
-        assertThat(nameInverter.inverter("lee")).isEqualTo("lee")
     }
 }
