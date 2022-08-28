b41ab74c2ab073cd533285da240c48561cced148
zionlee0927 <zionlee0927@tpirates.com>
2022-08-28 23:27:12 +0900

    TDD: Wrap extract class

--- src/main/kotlin/tdd/wordwrap/Wordwrap.kt
+++ src/main/kotlin/tdd/wordwrap/Wordwrap.kt
@@ -1,5 +1,7 @@
 package tdd.wordwrap
 
 class Wordwrap {
-
+    fun wrapGettingStuck(string: String, width: Int): String {
+        return if (string.length > width) string.replace(" ", "\n") else string
+    }
 }
--- src/test/kotlin/tdd/wordwrap/WordwrapTest.kt
+++ src/test/kotlin/tdd/wordwrap/WordwrapTest.kt
@@ -5,23 +5,21 @@ import org.junit.jupiter.api.Test
 
 
 internal class WordwrapTest{
+    private val wordwrap = Wordwrap()
 
     @Test
     fun wrapGettingStuck1(){
-        assertThat(wrapGettingStuck("word word", 4)).isEqualTo("word\nword")
+        assertThat(wordwrap.wrapGettingStuck("word word", 4)).isEqualTo("word\nword")
     }
 
     @Test
     fun wrapGettingStuck2(){
-        assertThat(wrapGettingStuck("a dog", 5)).isEqualTo("a dog")
+        assertThat(wordwrap.wrapGettingStuck("a dog", 5)).isEqualTo("a dog")
     }
 
     @Test
     fun wrapGettingStuck3(){
-        assertThat(wrapGettingStuck("a dog with a bone", 6)).isEqualTo("a dog\nwidth\n a\nbone")
+        assertThat(wordwrap.wrapGettingStuck("a dog with a bone", 6)).isEqualTo("a dog\nwidth\n a\nbone")
     }
 
-    private fun wrapGettingStuck(string: String, width: Int): String {
-        return if (string.length > width) string.replace(" ", "\n") else string
-    }
 }
